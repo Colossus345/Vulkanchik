fe6b507dcf17cb5262923c08b6746cd8adfcde4c
Colossus345 <47732393+Colossus345@users.noreply.github.com>
2021-11-01 15:32:50 +0700

    LoadOBJ model implemented with indexBuffer

--- Vulkanchik/CgeModel.cpp
+++ Vulkanchik/CgeModel.cpp
@@ -1,5 +1,28 @@
 #include "CgeModel.h"
 
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#include"Utils.h"
+#define GLM_ENABLE_EXPERIMENTAL
+#include<glm/gtx/hash.hpp>
+
+
+#include<iostream>
+#include<unordered_map>
+
+namespace std {
+	template<>
+	struct hash<cge::CgeModel::Vertex> {
+
+		size_t operator()(cge::CgeModel::Vertex const& vertex) const {
+			size_t seed = 0;
+			cge::hashCombine(seed, vertex.position, vertex.color, vertex.normal, vertex.uv);
+			return seed;
+		}
+	};
+}
+
 namespace cge {
 	CgeModel::CgeModel(CgeDevice& device, const CgeModel::Data& data) :cgeDevice{ device } {
 		createVertexBuffers(data.vertices);
@@ -14,6 +37,14 @@ namespace cge {
 			vkFreeMemory(cgeDevice.device(), indexBufferMemory, nullptr);
 		}
 
+	}
+
+	std::unique_ptr<CgeModel> CgeModel::createModelFromFile(CgeDevice& device, const std::string& filepath)
+	{
+		Data data{};
+		data.loadModel(filepath);
+		std::cout << "Vertex count-" << data.vertices.size() << "\n";
+		return std::make_unique<CgeModel>(device, data);
 	}
 
 	void CgeModel::bind(VkCommandBuffer commandBuffer)
@@ -140,5 +171,70 @@ namespace cge {
 		attributeDescriptions[1].offset = offsetof(Vertex,color);
 		return attributeDescriptions; 
 	}
+
+	void CgeModel::Data::loadModel(const std::string& filename)
+	{
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string warn, err;
+		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename.c_str())) {
+			throw std::runtime_error(warn + err);
+		}
+
+		vertices.clear();
+		indices.clear();
+
+		std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+		for (const auto& shape : shapes) {
+			for (const auto& index : shape.mesh.indices) {
+				Vertex vertex{};
+				
+				if (index.vertex_index >= 0) {
+					vertex.position = {
+						attrib.vertices[3 * index.vertex_index + 0],
+						attrib.vertices[3 * index.vertex_index + 1],
+						attrib.vertices[3 * index.vertex_index + 2],
+
+					};
+					auto color_index = 3 * index.vertex_index + 2;
+					if (color_index < attrib.colors.size()) {
+						vertex.color = {
+							attrib.colors[color_index - 2],
+							attrib.colors[color_index - 1],
+							attrib.colors[color_index -0],
+						};
+					}
+					else {
+						vertex.color = { 1.f,1.f,1.f };
+					}
+				}
+				if (index.normal_index  >= 0) {
+					vertex.normal = {
+						attrib.normals[3 * index.normal_index + 0],
+						attrib.normals[3 * index.normal_index + 1],
+						attrib.normals[3 * index.normal_index + 2],
+
+					};
+				}
+				if (index.texcoord_index  >= 0) {
+					vertex.uv = {
+						attrib.texcoords[2 * index.texcoord_index + 0],
+						attrib.texcoords[2 * index.texcoord_index + 1],
+
+					};
+				}
+
+
+				if (uniqueVertices.count(vertex) == 0) {
+					uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+					vertices.push_back(vertex);
+				}
+				indices.push_back(uniqueVertices[vertex]);
+			}
+		}
+	}
+
+
 
 }
--- Vulkanchik/CgeModel.h
+++ Vulkanchik/CgeModel.h
@@ -5,6 +5,7 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 
+#include<memory>
 #include<glm/glm.hpp>
 
 
@@ -18,19 +19,36 @@ namespace cge {
 		{
 			glm::vec3 position;
 			glm::vec3 color;
+			glm::vec3 normal{};
+			glm::vec2 uv{};
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+			bool operator==(const Vertex& other)const {
+				return position == other.position &&
+					color == other.color &&
+					normal == other.normal &&
+					uv == other.uv;
+
+			}
 		};
 		struct Data {
 			std::vector<Vertex> vertices{};
 			std::vector<uint32_t> indices{};
+
+			void loadModel(const std::string& filename);
 		};
 
 		CgeModel(CgeDevice &device,const CgeModel::Data &data);
 		~CgeModel();
 
+
+
 		CgeModel(const CgeModel&) = delete;
 		CgeModel& operator =(const CgeModel&) = delete;
+
+		static std::unique_ptr<CgeModel> createModelFromFile(CgeDevice& device, const std::string& filepath);
+
 
 		void bind(VkCommandBuffer commandBuffer);
 		void draw(VkCommandBuffer commandBuffer);
